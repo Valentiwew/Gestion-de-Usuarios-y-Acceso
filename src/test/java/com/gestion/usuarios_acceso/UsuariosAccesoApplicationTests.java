d4b1af20831d15fd4c5609c70dfe5e4e3eba0632
Valentiwew <bascunanbenjamin759@gmail.com>
2025-05-24 16:57:16 -0400

    GestionUsuariosV1

---
+++ src/test/java/com/gestion/usuarios_acceso/UsuariosAccesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.usuarios_acceso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosAccesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
